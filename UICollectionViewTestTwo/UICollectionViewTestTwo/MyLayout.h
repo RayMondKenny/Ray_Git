53e9bb06d40a4185144a38b2f083d16f8aa7d8cd
drake <2245237188@qq.com>
2021-08-01 10:21:10 +0800

    Secondt Commit

---
+++ UICollectionViewTestTwo/UICollectionViewTestTwo/MyLayout.h
@@ -0,0 +1,13 @@
+//
+//  MyLayout.h
+//  UICollectionViewTestTwo
+//
+//  Created by apple on 16/2/4.
+//  Copyright © 2016年 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+@interface MyLayout : UICollectionViewLayout
+//这个int值存储有多少个item
+@property(nonatomic,assign)int itemCount;
+@end
